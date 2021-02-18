5a76ce7b58ae1a84d7b212c6af8823c4c65c7fa9
Matt Schouten <matt.schouten@hidigital.solutions>
2021-02-18 16:02:08 -0600

    Refactor nested conditional into state transition table

--- extract_entries_from_markdown.py
+++ extract_entries_from_markdown.py
@@ -11,50 +11,69 @@ class STATE:
     FINISHED_TARGET_TABLE = 6
     DONE = 99
 
+class TRANSITION_LABELS:
+    START_STATE = 'START_STATE'
+    CONDITION_FUNCTION = 'CONDITION'
+    NEXT_STATE = 'NEXT_STATE'
+    SHOULD_START_NEW_TEXT = 'NEW_TEXT'
+    SHOULD_APPEND_TEXT = 'APPEND_TEXT'
+
 def is_table_line(line):
     return re.search("(?:\|.+\|)|(?:-{3,})", line)
+
+def not_table_line(line):
+    return not is_table_line(line)
 
 def is_target_label_heading(line, label_text):
     return label_text and re.search("#+.*{}".format(label_text), line)
 
+def get_state_transitions(target_heading_text):
+    
+    def is_target_label_heading_curry(line):
+        return is_target_label_heading(line, target_heading_text)
+   
+    STATE_TRANSITION_LABELS = [
+        TRANSITION_LABELS.START_STATE,
+        TRANSITION_LABELS.CONDITION_FUNCTION,
+        TRANSITION_LABELS.NEXT_STATE,
+        TRANSITION_LABELS.SHOULD_START_NEW_TEXT,
+        TRANSITION_LABELS.SHOULD_APPEND_TEXT
+    ]
+
+    STATE_TRANSITIONS = [
+        [STATE.WATCHING_WAITING, is_table_line, STATE.IN_TABLE, True, True],
+        [STATE.WATCHING_WAITING, is_target_label_heading_curry, STATE.TARGET_LABEL_FOUND, False, False],
+        [STATE.IN_TABLE, is_table_line, STATE.IN_TABLE, False, True],
+        [STATE.IN_TABLE, not_table_line, STATE.FINISHED_UNLABELED_TABLE, False, False],
+        [STATE.FINISHED_UNLABELED_TABLE, is_target_label_heading_curry, STATE.TARGET_LABEL_FOUND, False, False],
+        [STATE.TARGET_LABEL_FOUND, is_table_line, STATE.IN_TARGET_TABLE, True, True],
+        [STATE.IN_TARGET_TABLE, is_table_line, STATE.IN_TARGET_TABLE, False, True],
+        [STATE.IN_TARGET_TABLE, not_table_line, STATE.FINISHED_TARGET_TABLE, False, False]
+    ]
+    transition_dictionaries = [dict(zip(STATE_TRANSITION_LABELS, transition)) for transition in STATE_TRANSITIONS]
+    starting_states = list(set([td[TRANSITION_LABELS.START_STATE] for td in transition_dictionaries]))
+    transitions = {k: [td for td in transition_dictionaries if td[TRANSITION_LABELS.START_STATE] == k] for k in starting_states}
+    return transitions
+
 def get_table_text(markdown, target_heading_text = None):
+    TRANSITIONS = get_state_transitions(target_heading_text)
+
     md_lines = markdown.splitlines()
     state = STATE.WATCHING_WAITING
     table_text = []
 
     for line in md_lines:
-        if state == STATE.WATCHING_WAITING:
-            if is_table_line(line):
-                state = STATE.IN_TABLE
-                table_text = [line]
-                continue
+        possible_transitions = TRANSITIONS[state]
 
-        if state == STATE.IN_TABLE:
-            if (is_table_line(line)):
-                table_text.append(line)
-                continue
-            else:
-                state = STATE.FINISHED_UNLABELED_TABLE
-                continue
-        
-        if state in [STATE.FINISHED_UNLABELED_TABLE, STATE.WATCHING_WAITING]:
-            if (is_target_label_heading(line, target_heading_text)):
-                state = STATE.TARGET_LABEL_FOUND
+        for transition in possible_transitions:
+            if (transition[TRANSITION_LABELS.CONDITION_FUNCTION](line)):
+                state = transition[TRANSITION_LABELS.NEXT_STATE]
+                if transition[TRANSITION_LABELS.SHOULD_START_NEW_TEXT]: table_text = []
+                if transition[TRANSITION_LABELS.SHOULD_APPEND_TEXT]: table_text.append(line)
                 continue
         
-        if state == STATE.TARGET_LABEL_FOUND:
-            if (is_table_line(line)):
-                table_text = [line]
-                state = STATE.IN_TARGET_TABLE
-                continue
-        
-        if state == STATE.IN_TARGET_TABLE:
-            if (is_table_line(line)):
-                table_text.append(line)
-                continue
-            else:
-                state = STATE.FINISHED_TARGET_TABLE
-                continue
+        if state not in TRANSITIONS:
+            break
 
     table_lines = [line.strip() for line in table_text]
 
